3917829488dcb45f0c92456b35540ab16805aa35
haruki <meteo.haru@gmail.com>
2024-03-20 11:41:24 +0000

    add export

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import './sass/global.scss'
 
 export { Button } from './components/Button/Button'
 export { Typo } from './components/Typography/Typo'
+export { Input } from './components/Input/Input'
+export { SearchInput } from './components/SearchInput/SearchInput'
 
 
 // ====================
